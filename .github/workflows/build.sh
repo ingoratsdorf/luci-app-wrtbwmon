4b4cdce4f88fbc7369689f72f8170acf117e1a5f
Ingo Ratsdorf <ingo@envirology.co.nz>
2021-02-11 19:47:31 +1300

    Initial commit

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,9 +3,9 @@
 sed -i 's/git\.openwrt\.org\/project\/luci/github\.com\/openwrt\/luci/g' ./feeds.conf.default
 ./scripts/feeds update luci
 ./scripts/feeds install luci
-mv ./bin/luci-app-wrtbwmon ./package/
+mv ./bin/luci-app-wrtbwmon-ng ./package/
 make defconfig
-make package/luci-app-wrtbwmon/compile V=s -j$(nproc) BUILD_LOG=1
+make package/luci-app-wrtbwmon-ng/compile V=s -j$(nproc) BUILD_LOG=1
 
 tar -cJf logs.tar.xz logs
 mv logs.tar.xz bin
--- luci-app-wrtbwmon-ng/luasrc/controller/wrtbwmon-ng.lua
+++ luci-app-wrtbwmon-ng/luasrc/controller/wrtbwmon-ng.lua
@@ -1,20 +1,20 @@
-module("luci.controller.wrtbwmon", package.seeall)
+module("luci.controller.wrtbwmon-ng", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/wrtbwmon") then
+	if not nixio.fs.access("/etc/config/wrtbwmon-ng") then
 		return
 	end
 
 	entry({"admin", "network", "usage"},
 		alias("admin", "network", "usage", "details"),
-		 _("Traffic Status"), 60).acl_depends={ "luci-app-wrtbwmon" }
+		 _("Traffic Status"), 60).acl_depends={ "luci-app-wrtbwmon-ng" }
 	entry({"admin", "network", "usage", "details"},
-		view("wrtbwmon/details"),
+		view("wrtbwmon-ng/details"),
 		_("Details"), 10)
 	entry({"admin", "network", "usage", "config"},
-		view("wrtbwmon/config"),
+		view("wrtbwmon-ng/config"),
 		_("Configuration"), 20)
 	entry({"admin", "network", "usage", "custom"},
-		view("wrtbwmon/custom"),
+		view("wrtbwmon-ng/custom"),
 		_("User file"), 30)
 end
